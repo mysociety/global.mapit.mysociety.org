d89202c27cbd702f4fefcfd4c58810262ec07528
Matthew Somerville <matthew-github@dracos.co.uk>
2017-04-04 16:37:57 +0100

    Port area list indent across from upstream.

---
+++ mapit_global/templatetags/display.py
@@ -0,0 +1,20 @@
+import re
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def css_indent_class(area):
+    """Get a CSS class for use on <li> representations of this area
+
+    Currently this is only used to indicate the indentation level
+    that should be used on the code types O02, O03, O04 ... O011,
+    used by global MapIt.
+    """
+    m = re.search(r'^O([01][0-9])$', area.type.code)
+    if m:
+        return "area_level_%s" % m.group(1)
+    else:
+        return ""
